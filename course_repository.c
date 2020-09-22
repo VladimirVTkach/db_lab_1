ea548f77a4ab48fd9308ea04ade3b403d3290b25
Tkach Vladimir <vladimirv.tkach@gmail.com>
2020-09-22 18:00:08 +0300

    get_s is now use get_m to find appropriate course

--- course_repository.c
+++ course_repository.c
@@ -71,91 +71,44 @@ struct Course *get_m(int course_id) {
 }
 
 struct Group *get_s(int course_id, int group_id) {
-    FILE *courses_index_file = fopen(COURSES_INDEX_FILE_PATH, "r");
-    if (courses_index_file == 0) {
-        printf("index file not found");
-        return 0;
-    }
+    struct Course *course = get_m(course_id);
 
-    size_t index_file_size = get_file_size(COURSES_INDEX_FILE_PATH);
-    size_t course_structure_size = sizeof(struct Course);
-    size_t index_structure_size = sizeof(struct CourseIndex);
+    if (course != 0) {
+        size_t group_structure_size = sizeof(struct Group);
 
-    struct CourseIndex *index_buffer = malloc(index_file_size);
-    size_t index_items_read_cnt = fread(index_buffer, index_structure_size, index_file_size, courses_index_file);
-    if (index_items_read_cnt < index_structure_size / index_file_size) {
-        printf("error while reading index file occurred");
-        free(index_buffer);
-        fclose(courses_index_file);
-        return 0;
-    }
-    fclose(courses_index_file);
-
-    struct CourseIndex key = {course_id, -1};
-    struct CourseIndex *index = bsearch(&key,
-                                        index_buffer,
-                                        index_file_size / index_structure_size,
-                                        index_structure_size,
-                                        compare_index);
-    if (index == 0) {
-        free(index_buffer);
-        return 0;
-    }
-
-    long main_file_address = index->address;
-    free(index_buffer);
-
-    FILE *courses_file = fopen(COURSES_FILE_PATH, "r");
-    if (courses_file == 0) {
-        printf("courses file not found");
-        return 0;
-    }
-    fseek(courses_file, main_file_address, SEEK_SET);
-
-    struct Course *course = malloc(course_structure_size);
-    size_t data_items_read_cnt = fread(course, course_structure_size, 1, courses_file);
-    if (data_items_read_cnt != 1) {
-        printf("error while reading courses file occurred");
-        fclose(courses_file);
+        int group_address = course->group_address;
         free(course);
-        return 0;
-    }
-    fclose(courses_file);
 
-    FILE *groups_file = fopen(GROUPS_FILE_PATH, "r");
-    if (groups_file == 0) {
-        printf("groups file not found");
-        free(course);
-        return 0;
-    }
-
-    size_t groups_file_size = get_file_size(GROUPS_FILE_PATH);
-    size_t group_structure_size = sizeof(struct Group);
-
-    int group_address = course->group_address;
-    free(course);
-
-    struct Group *group = malloc(group_structure_size);
-    while (group_address != -1) {
-        fseek(groups_file, group_address, SEEK_SET);
-        size_t group_items_read_cnt = fread(group, group_structure_size, 1, groups_file);
-        if (group_items_read_cnt != 1) {
-            printf("error while reading groups file occurred");
-            fclose(groups_file);
+        FILE *groups_file = fopen(GROUPS_FILE_PATH, "r+");
+        if (groups_file == 0) {
+            printf("groups file not found");
             return 0;
         }
 
-        if (group->group_id == group_id) {
-            fclose(groups_file);
-            return group;
+        struct Group *group = malloc(group_structure_size);
+        while (group_address != -1) {
+            fseek(groups_file, group_address, SEEK_SET);
+            size_t group_items_read_cnt = fread(group, group_structure_size, 1, groups_file);
+            if (group_items_read_cnt != 1) {
+                printf("error while reading groups file occurred");
+                fclose(groups_file);
+                return 0;
+            }
+
+            if (group->group_id == group_id && group->is_deleted == 0) {
+                fclose(groups_file);
+                return group;
+            }
+
+            group_address = group->next_group_address;
         }
 
-        group_address = group->next_group_address;
-    }
-
-    if (group->group_id == group_id) {
-        fclose(groups_file);
-        return group;
+        if (group->group_id == group_id && group->is_deleted == 0) {
+            fclose(groups_file);
+            return group;
+        } else {
+            return 0;
+        }
     } else {
         return 0;
     }
